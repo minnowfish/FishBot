ed66ffb2c34a27e19d5d059f9a0c20de4963f63d
Ashlyn <ashlynchor15@gmail.com>
2024-05-25 09:45:21 +0800

    identify hold piece

--- GetScreen.py
+++ GetScreen.py
@@ -8,13 +8,18 @@ HEIGHT = 525
 
 #colors of each tetris piece
 class tetromino(Enum):
-    I = ()
-    O = ()
-    T = ()
-    S = ()
-    Z = ()
-    L = ()
+    I = (74, 163, 180, 255)
+    O = (138, 131, 53, 255)
+    T = (112, 39, 135, 255)
+    S = (69, 137, 72, 255)
+    Z = (128, 42, 37, 255)
+    L = (134, 106, 47, 255)
     J = (49, 89, 136, 255)
+
+#initalise variables/arrays
+board = [['' for _ in range(10)] for _ in range(20)]
+hold = ''
+queue = []
 
 def capture_screenshot():
     #get the screenshot of the current state of the game
@@ -29,28 +34,33 @@ def capture_screenshot():
     hold_image = screenshot.crop((60, 104, 61, 105))
     hold_image.save('hold.png')
 
-def analyze_board(screenshot, WIDTH, HEIGHT):
+def analyze_board(WIDTH, HEIGHT):
     cell_width = WIDTH // 10
     cell_height = HEIGHT // 20
 
-    for row in range(20):
-        y = (cell_height * row) + cell_height//2
-        for col in range(10):
-            x = (cell_width * col) + cell_width//2
-            colour = screenshot.getpixel((x,y))
-            if colour != (0,0,0,255):
-                board[row][col] = 1
-            else: 
-                board[row][col] = 0
+    #store board state in an array
+    with Image.open('board_state.png') as board_state:
+        for row in range(20):
+            y = (cell_height * row) + cell_height//2
+            for col in range(10):
+                x = (cell_width * col) + cell_width//2
+                colour = board_state.getpixel((x,y))
+                if colour != (0,0,0,255):
+                    board[row][col] = 1
+                else: 
+                    board[row][col] = 0
+    
+    #identify hold piece and store in a variable
+    with Image.open('hold.png') as hold_image:
+        colour = hold_image.getpixel((0,0))
+        hold = tetromino(colour).name
+    return hold
 
+#screenshot = capture_screenshot()
 
-#initalise 2D array to hold the tetris board
-board = [['' for _ in range(10)] for _ in range(20)]
+hold = analyze_board(WIDTH, HEIGHT)
 
-screenshot = capture_screenshot()
-
-analyze_board(Image.open('board_state.png'), WIDTH, HEIGHT)
-
+#to be removed
 for i in range(20):
     for j in range(10):
         if board[i][j] == 0:
@@ -58,3 +68,5 @@ for i in range(20):
         else:
             print("[]", end = "")
     print("")
+
+print(f"\nhold piece : {hold}")
--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-import pyautogui
-from PIL import Image
-
-hold = Image.open('hold.png')
-print(hold.getpixel((0,0)))
+#empty for now
 
